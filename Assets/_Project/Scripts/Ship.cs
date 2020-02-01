b978c60713b78f8c9c4f870e61c0e39b1f4c7462
worthingtonnl <worthingtonnl@gmail.com>
2020-01-31 23:15:17 -0700

    ship logic

---
+++ Assets/_Project/Scripts/Ship.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ship : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
