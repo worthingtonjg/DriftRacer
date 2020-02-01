2a37dcac02cd41284d0ec5131c9dd8b8a603566d
worthingtonnl <worthingtonnl@gmail.com>
2020-02-01 13:28:49 -0700

    web build

--- Assets/_Project/Scripts/Player.cs
+++ Assets/_Project/Scripts/Player.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    private MeshCollider collider;
     private Rigidbody body;
     private bool Dead;
     private bool boosted;
     private Image healthbarimage;
+    private Image ammobarimage;
 
     public enum EnumShipType
     {
@@ -26,21 +28,30 @@ public class Player : MonoBehaviour
     public GameObject Explosion;
     public GameObject CountDown;
     public GameObject healthbar;
+    public GameObject ammobar;
     public GameObject damaged;
     public GameObject WeaponPort;
     public GameObject Bullet;
     public float maxhealth = 100f;
     public float health = 100f;
+    public float ammo = 5f;
+    public float maxammo = 5f;
     public float damage = 25f;
     public float bulletSpeed = 5000f;
     public float fireRate = 1f;
     public float lastFireTime;
+    public bool Shooting;
+
     // Start is called before the first frame update
     void Start()
     {
         Dead = true;
         body = GetComponent<Rigidbody>();
+        collider = GetComponent<MeshCollider>();
+
         healthbarimage = healthbar.GetComponent<Image>();
+        ammobarimage = ammobar.GetComponent<Image>();
+
         StartCoroutine(GameStart());
     }
 
@@ -66,7 +77,14 @@ public class Player : MonoBehaviour
             }
         }
 
-        if(!boosted)
+        if(boosted)
+        {
+            if (body.velocity.magnitude > maxspeed * 1.5f)
+            {
+                body.velocity = body.velocity.normalized * (maxspeed * 1.5f);
+            }
+        }
+        else
         {
             if (body.velocity.magnitude > maxspeed)
             {
@@ -92,7 +110,8 @@ public class Player : MonoBehaviour
             }
             if(Input.GetKey(KeyCode.LeftShift))
             {
-                Shoot();
+                StartCoroutine(Shoot());
+
             }
         }
         else
@@ -109,18 +128,33 @@ public class Player : MonoBehaviour
             }
             if(Input.GetKey(KeyCode.RightShift))
             {
-                Shoot();
+                StartCoroutine(Shoot());
             }            
         }
     }
 
-    private void Shoot()
+    private IEnumerator Shoot()
     {
+        if (ammo <= 0) yield break;
+        if (Shooting) yield break;
+        Shooting = true;
+
         var bulletPrefab = GameObject.Instantiate(Bullet, WeaponPort.transform.position, Quaternion.identity);
         var bulletBody = bulletPrefab.GetComponent<Rigidbody>();
-                
+
+        ammo = ammo - 1;
+        if (ammo < 0)
+        {
+            ammo = 0;
+        }
+        ammobarimage.fillAmount = ammo / maxammo;
+
+
         bulletBody.AddForce(transform.forward * bulletSpeed);
-        Destroy(bulletPrefab, 3f);        
+        Destroy(bulletPrefab, 3f);
+
+        yield return new WaitForSeconds(fireRate);
+        Shooting = false;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -160,6 +194,12 @@ public class Player : MonoBehaviour
                     }
                     break;
                 case "Upgrade":
+                    if (ammo <= maxammo)
+                    {
+                        ammo += 1;
+                        ammobarimage.fillAmount = ammo / maxammo;
+                    }
+
                     break;
             }
             print(other.name);
@@ -183,6 +223,9 @@ public class Player : MonoBehaviour
 
         boosted = false;
         Dead = true;
+
+        collider.enabled = false;
+        body.detectCollisions = false;
         body.velocity = Vector3.zero;
         GetComponent<Renderer>().enabled = false;
         TextMeshProUGUI countDownText = CountDown.GetComponent<TextMeshProUGUI>();
@@ -210,6 +253,9 @@ public class Player : MonoBehaviour
 
         //transform.position = new Vector3(Spawn.transform.position.x, transform.position.y, Spawn.transform.position.z);
         GetComponent<Renderer>().enabled = true;
+        body.detectCollisions = true;
+        collider.enabled = true;
+
         Dead = false;
         health = maxhealth;
         UpdateHealthBar();
@@ -237,7 +283,7 @@ public class Player : MonoBehaviour
         if(boosted) yield break;
 
         boosted = true;
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(.5f);
         boosted = false;
     }
 }
