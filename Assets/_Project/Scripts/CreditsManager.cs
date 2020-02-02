5537668f44ef79949228242b7bb1368148255681
worthingtonnl <worthingtonnl@gmail.com>
2020-02-01 17:17:20 -0700

    skybox

---
+++ Assets/_Project/Scripts/CreditsManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsManager : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("01MainMenu");
+    }
+
+}
